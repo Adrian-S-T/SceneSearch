6a8deb103386d73222c1bf3658b753eeaf853aa5
Adrian-ST <todorescu.adrian06@gmail.com>
2026-07-18 12:19:15 +0300

    Add Scene data model

---
+++ src/scene.py
@@ -0,0 +1,7 @@
+class Scene:
+
+    def __init__(self, anime, episode, timestamp, description):
+        self.anime = anime
+        self.episode = episode
+        self.timestamp = timestamp
+        self.description = description
