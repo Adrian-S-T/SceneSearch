a3b76d1ace1fbaf47e436a574cbbf5c10aa3d059
Adrian-ST <todorescu.adrian06@gmail.com>
2026-08-04 10:13:24 +0300

    added nltk tokenizer for search preprocessing

--- src/search.py
+++ src/search.py
@@ -1,13 +1,15 @@
 from display import scene_printer
 from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 stemmer = PorterStemmer()
 def search_scenes(scenes,keyword):
     """searches for scenes in anime names and desc based on inputted keywords + counts matches"""
-    search_words={stemmer.stem(word) for word in (keyword.lower()).split()}
+    search_words={stemmer.stem(word) for word in word_tokenize((keyword.lower()))}
     results=[]
     for element in scenes:
-          name = {stemmer.stem(word) for word in element.anime.lower().split()}
-          description = {stemmer.stem(word) for word in element.description.lower().split()}
+          score=0
+          name = {stemmer.stem(word) for word in word_tokenize(element.anime.lower())}
+          description = {stemmer.stem(word) for word in word_tokenize(element.description.lower())}
           for word in search_words:
              if word in name:
                       score=score+3
