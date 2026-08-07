71afb92ee24e58859b56fe493878329ce8106ce7
Adrian-ST <todorescu.adrian06@gmail.com>
2026-08-07 13:49:38 +0300

    added a helper function for the searching algorithm

--- src/search.py
+++ src/search.py
@@ -2,14 +2,16 @@ from display import scene_printer
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 stemmer = PorterStemmer()
+def process_text(text):
+      return {stemmer.stem(word) for word in word_tokenize(text.lower())}
 def search_scenes(scenes,keyword):
     """searches for scenes in anime names and desc based on inputted keywords + counts matches"""
-    search_words={stemmer.stem(word) for word in word_tokenize((keyword.lower()))}
+    search_words=process_text(keyword)
     results=[]
     for element in scenes:
-          score=0
-          name = {stemmer.stem(word) for word in word_tokenize(element.anime.lower())}
-          description = {stemmer.stem(word) for word in word_tokenize(element.description.lower())}
+          score=0 
+          name = process_text(element.anime)
+          description = process_text(element.description)
           for word in search_words:
              if word in name:
                       score=score+3
