23b17480573b133e6fc748774c0bf5d5e987ef3e
Adrian-ST <todorescu.adrian06@gmail.com>
2026-08-10 12:06:32 +0300

    improved the scoring system

--- src/search.py
+++ src/search.py
@@ -3,20 +3,18 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 stemmer = PorterStemmer()
 def process_text(text):
-      return {stemmer.stem(word) for word in word_tokenize(text.lower())}
+      return [stemmer.stem(word) for word in word_tokenize(text.lower())]
 def search_scenes(scenes,keyword):
     """searches for scenes in anime names and desc based on inputted keywords + counts matches"""
-    search_words=process_text(keyword)
+    search_words={stemmer.stem(word) for word in word_tokenize(keyword.lower())}
     results=[]
     for element in scenes:
           score=0 
           name = process_text(element.anime)
           description = process_text(element.description)
           for word in search_words:
-             if word in name:
-                      score=score+3
-             if word in description:
-                      score=score+1
+             score += 3 * name.count(word)
+             score += description.count(word)
           if score>0:
              results.append((element,score))
     return results
